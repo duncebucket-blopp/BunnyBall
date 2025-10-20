baaa3d874f4f15bdde1bae66d487626cfeed255d
duncebucket-blopp <mikelcalliste4@gmail.com>
2025-10-20 09:38:31 -0400

    inital commit

---
+++ BunnyBall/Assets/UnityTechnologies/BunnyBall/Scripts/Key.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] GameObject shockwavePrefab;
+    [SerializeField] GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            gameManager.gameOver = true;
+            Instantiate(shockwavePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject, 0.1f);
+        }
+    }
+}
+
+// check if we collide with the player
+if (other.CompareTag("Player")) {
+// sets game over to true
+gameManager.gameOver = true;
+//creates a shockwave
+Instantiate(shockwavePrefab, transform.position, Quaternion.
+// destroys key
+Destroy(gameObject, t:0.1f);
+
+}
